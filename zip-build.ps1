a18fe8092b0c2ff55f39aa3ee9a8d40538f0b3a1
betapacketdlldink.nodllforyou <107498533+RealPacket@users.noreply.github.com>
2024-03-30 11:16:55 -0500

    Fix releases

---
+++ zip-build.ps1
@@ -0,0 +1,23 @@
+$InputFolder = "build"
+
+$InputFolderName = Split-Path -Path $InputFolder -Leaf
+$ZipFileName = $InputFolderName + ".zip"
+$ZipFilePath = $ZipFileName
+$ZIP_BYTES = (80, 75, 05, 06, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 001)
+
+
+# Check if the zip file exists and create it if not
+if (-Not (Test-Path -Path $ZipFilePath)) {
+    New-Item -Path $ZipFilePath -ItemType File
+    Set-Content -Path $ZipFilePath -Value $ZIP_BYTES -Encoding Byte
+}
+
+# Loop through each subfolder in the input folder
+$Subfolders = Get-ChildItem $InputFolder | Where-Object { $_.PSIsContainer }
+ForEach ($Subfolder in $Subfolders) {
+    # Get the subfolder path
+    $SubfolderPath = $Subfolder.FullName
+
+    # Add the subfolder to the zip file
+    Compress-Archive -Path $SubfolderPath -DestinationPath $ZipFilePath -Update
+}
